1f1a55f7f484486a48c0835187cceb86369d77fd
Fiona <fionajayne.potter@gmail.com>
2024-02-10 17:35:10 +0100

    Créer fichier db.js + route company getAll

--- back/app.js
+++ back/app.js
@@ -1,6 +1,5 @@
 // Imports
 const express = require('express');
-const { createClient } = require('@supabase/supabase-js');
 const bodyParser = require('body-parser');
 
 // Créer une application Express
@@ -9,12 +8,6 @@ const app = express();
 // Bodyparser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-// Créer un client supabase pour la connexion à la BDD
-const supabase = createClient(
-    'https://krsjgrhjzlaybybwedee.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtyc2pncmhqemxheWJ5YndlZGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc1MTIyOTUsImV4cCI6MjAyMzA4ODI5NX0.BqVYL5l-oYAnXquedaQP2VGOPKImWMl5BbbJkXu9mIg'
-);
 
 // Définition des routes
 const companyRoutes = require('./routes/company');
--- back/controllers/company.js
+++ back/controllers/company.js
@@ -1,8 +1,21 @@
+const supabase = require('../db');
+
 // Récupération de tous les entreprises
 exports.getAll = (req, res, next) => {
     supabase
     .from('company')
     .select('*')
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+    .then(
+        (data) => {
+            res.status(200).json({
+                message: 'Entreprises récupérées'
+            })
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            })
+        }
+    );
 }
