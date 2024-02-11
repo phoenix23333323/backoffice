ea31b39357a47e300404eb53f64d68eb9c9b7fa5
Fiona <fionajayne.potter@gmail.com>
2024-02-11 14:00:50 +0100

    Mise en place d'un validateur d'email

---
+++ back/middlewares/email-validator.js
@@ -0,0 +1,11 @@
+const validator = require('validator');
+
+// Validationdu format de l'email
+module.exports = (req, res, next) => {
+	const email = req.body.email;
+	if (validator.isEmail(email) === true) {
+		next();
+	} else {
+		return res.status(400).json({ error: 'Le format de l\'email est invalide' })
+	}
+}
--- back/routes/users.js
+++ back/routes/users.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 // Middlewares
 const limitMax = require('../middlewares/limiter');
+const emailValidator = require('../middlewares/email-validator');
 
 // Controller
 const usersCtrl = require('../controllers/users');
 
 // Router
-router.post('/signup', usersCtrl.signup);
+router.post('/signup', emailValidator, usersCtrl.signup);
 router.post('/signin', limitMax.limiter, usersCtrl.signin);
 
 module.exports = router;
