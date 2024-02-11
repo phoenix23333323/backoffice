ebb2172a8154fd73868e2f941b3cbecf9e5bbfd5
Fiona <fionajayne.potter@gmail.com>
2024-02-11 13:56:14 +0100

    Mise en place d'un limiter de connexion

---
+++ back/middlewares/limiter.js
@@ -0,0 +1,10 @@
+const rateLimit = require('express-rate-limit')
+
+// rate limiter
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	max: 5, // Limite a 5 requetes sur 5min
+	message: 'Trop de tentatives de connexion, essayez à nouveau dans 5 min',
+})
+
+module.exports = { limiter }
--- back/routes/users.js
+++ back/routes/users.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const router = express.Router();
 
+// Middlewares
+const limitMax = require('../middlewares/limiter');
+
 // Controller
 const usersCtrl = require('../controllers/users');
 
 // Router
 router.post('/signup', usersCtrl.signup);
-router.post('/signin', usersCtrl.signin);
+router.post('/signin', limitMax.limiter, usersCtrl.signin);
 
 module.exports = router;
