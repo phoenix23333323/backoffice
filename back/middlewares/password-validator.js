969c889cd1cbf4734330c4967864a40b188e9d34
Fiona <fionajayne.potter@gmail.com>
2024-02-11 14:07:13 +0100

    Mise en place d'un contrôle du mot de passe

---
+++ back/middlewares/password-validator.js
@@ -0,0 +1,23 @@
+const passwordValidator = require('password-validator');
+
+// Schema de validation :
+const passwordSchema = new passwordValidator();
+passwordSchema
+	.is().min(8) // Minimum length 8
+	.is().max(30) // Maximum length 30
+	.has().uppercase() // Doit contenir au moins un majuscule
+	.has().lowercase() // Doit contenir au moins un minuscule
+	.has().digits(1) // Doit contenir au moins un chiffre
+	.has().not().spaces() // Ne doit pas contenir d'espace
+	.is().not().oneOf(['1=1', 'OR a=a', '=', 'OR 1=1', 'Password123', 'Azerty123']) // Ne doit pas être une de ces valeurs
+
+// Test du mot de passe saisit par l'utilisateur
+module.exports = (req, res, next) => {
+	if(passwordSchema.validate(req.body.password)) {
+		next();
+	} else {
+		return res.status(400).json({
+			error: "Le mot de passe n'est pas conforme, il doit contenir entre 8 et 30 caractères, au moins 1 majuscule, 1 minuscule, 1 chiffre. Les espaces sont interdits."
+		})
+	}
+}
--- back/routes/users.js
+++ back/routes/users.js
@@ -5,12 +5,13 @@ const router = express.Router();
 // Middlewares
 const limitMax = require('../middlewares/limiter');
 const emailValidator = require('../middlewares/email-validator');
+const passwordValidator = require('../middlewares/password-validator');
 
 // Controller
 const usersCtrl = require('../controllers/users');
 
 // Router
-router.post('/signup', emailValidator, usersCtrl.signup);
+router.post('/signup', emailValidator, passwordValidator, usersCtrl.signup);
 router.post('/signin', limitMax.limiter, usersCtrl.signin);
 
 module.exports = router;
